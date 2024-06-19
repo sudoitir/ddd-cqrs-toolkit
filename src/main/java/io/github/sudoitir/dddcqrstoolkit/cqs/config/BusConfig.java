7428170ec8d3cfe53f7c4f24a140a96fddab41b4
Amirabdollahi <sudoit.ir@gmail.com>
2024-06-19 23:30:37 +0330

    feat: Autoconfiguration

--- src/main/java/io/github/sudoitir/dddcqrstoolkit/cqs/config/BusConfig.java
+++ src/main/java/io/github/sudoitir/dddcqrstoolkit/cqs/config/BusConfig.java
@@ -6,10 +6,14 @@ import io.github.sudoitir.dddcqrstoolkit.cqs.module.ValidationModule;
 import jakarta.validation.Validator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
+@ComponentScan ("io.github.sudoitir.dddcqrstoolkit")
+@EnableAspectJAutoProxy (proxyTargetClass = true)
 public class BusConfig {
 
     @Bean
