0f3b4ddc0ab7427ab07cffb8a9506c74a3ca8ae1
Amirabdollahi <sudoit.ir@gmail.com>
2024-06-19 10:04:32 +0330

    feat: Add BusConfig

---
+++ src/main/java/io/github/sudoitir/dddcqrstoolkit/cqs/config/BusConfig.java
@@ -0,0 +1,22 @@
+package io.github.sudoitir.dddcqrstoolkit.cqs.config;
+
+import io.github.sudoitir.dddcqrstoolkit.cqs.module.LoggingModule;
+import io.github.sudoitir.dddcqrstoolkit.cqs.module.ModuleRegistry;
+import io.github.sudoitir.dddcqrstoolkit.cqs.module.ValidationModule;
+import jakarta.validation.Validator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BusConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ModuleRegistry moduleRegistry(Validator validator) {
+        ValidationModule validationModule = new ValidationModule(validator);
+        LoggingModule loggingModule = new LoggingModule();
+        return new ModuleRegistry(validationModule, loggingModule);
+    }
+
+}
